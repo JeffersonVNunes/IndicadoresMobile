5c1af1312477465a733433c9a3d0a94d775c52d9
JeffersonVNunes <jeffersonvnunes@gmail.com>
2015-05-18 01:59:10 -0300

    Adicionado template modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,47 @@
 angular.module('indimobile.controllers',[])
 
-.controller('EntradaCtrl', function($scope, $state, TaskServer) {
+.controller('EntradaCtrl', function($scope, $ionicPopup, $state, TaskServer) {
   $scope.servidor = TaskServer.servidor;
 
-  $scope.submitForm = function(id_projeto) {
-    TaskServer.autenticar(id_projeto).then(function(res){
-
-      TaskServer.id = res.data.Id;
-      TaskServer.descricao = res.data.Descricao;
-      TaskServer.data_inicio = res.data.DataInicio;
-      TaskServer.data_termino = res.data.DataTermino;
-
-      $state.go('indi-menu');
-
-    }, function() {
-      // error handling here
-      alert('Projeto não encontrado');
+  $scope.showAlert = function(mensagem) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'Aviso',
+      template: mensagem
     });
   }
+
+  $scope.submitForm = function(id_projeto) {
+
+    if ( !isNaN(id_projeto)) {
+      TaskServer.autenticar(id_projeto).then(function (res) {
+
+        TaskServer.id = res.data.id;
+        TaskServer.descricao = res.data.descricao;
+        TaskServer.data_inicio = res.data.datainicio;
+        TaskServer.data_termino = res.data.datatermino;
+
+        $state.go('indi-menu');
+
+      }, function () {
+        // error handling here
+        $scope.showAlert('Projeto não encontrado');
+      });
+    }
+    else{
+      $scope.showAlert('Informe o id do projeto.');
+    }
+  }
 })
-.controller('MenuCtrl', function($scope, $state, projeto) {
+.controller('MenuCtrl', function($scope, $ionicPopup, $ionicModal, $state, projeto) {
   $scope.projeto = projeto;
   $scope.dados = 'Não Carregado';
+
+  $scope.showAlert = function(mensagem) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'Aviso',
+      template: mensagem
+    });
+  };
 
   $scope.getIndicadores = function(){
     projeto.getIndicadores().then(function(res){
@@ -30,7 +50,42 @@ angular.module('indimobile.controllers',[])
 
     }, function() {
       // error handling here
-      alert('Projeto não encontrado');
+      $scope.showAlert('Projeto não encontrado');
     });
+
+    $ionicModal.fromTemplateUrl('templates/velochart.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function($ionicModal) {
+      $scope.modal = $ionicModal
+    });
+
+    $scope.openModal = function(sel_indi) {
+      $scope.sel_indi = sel_indi;
+
+      $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+
+  };
+
+  $scope.getCor = function(status){
+    if(status.substr(0,2).toUpperCase() == 'NO'){
+      return 'bc-verde';
+    }else if(status.substr(0,2).toUpperCase() == 'AL'){
+      return 'bc-amarelo';
+    }else if(status.substr(0,2).toUpperCase() == 'CR'){
+      return 'bc-vermelho';
+    }else{
+      return '';
+    }
+
   }
 });
